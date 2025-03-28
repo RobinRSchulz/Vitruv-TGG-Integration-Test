b6a93e966816914a27a2e741d2c9213cb5f7fac4
Robin Schulz <uyduk@student.kit.edu>
2025-03-28 13:23:27 +0100

    Add insurancefamiliespersons package

---
+++ vsum/src/test/java/tools/vitruv/methodologisttemplate/vsum/EvaluationTest.java
@@ -0,0 +1,15 @@
+package tools.vitruv.methodologisttemplate.vsum;
+
+import edu.kit.ipd.sdq.metamodels.families.FamiliesFactory;
+import edu.kit.ipd.sdq.metamodels.insurance.InsuranceFactory;
+
+/**
+ * This represents (parts of) the evaluation, which for convenience, is implemented with junit.
+ */
+public class EvaluationTest {
+
+    public void testBasicEvaluation() {
+
+
+    }
+}
